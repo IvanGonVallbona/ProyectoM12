4896e0738a1fb534ad2dad07d96e800fd2bfaf7a
[Genis] <genis_manuelferran@iescarlesvallbona.cat>
2025-04-01 18:35:21 +0200

    Creacio CRUD Esdeveniments

--- Back-end/ProyectoM12/app/Models/Esdeveniment.php
+++ Back-end/ProyectoM12/app/Models/Esdeveniment.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Esdeveniment extends Model
 {
-    //
+        protected $fillable = [
+        'nom',
+        'descripcio',
+        'data',
+        'tipus',
+        'user_id',
+    ];
+    
+    protected $casts = [
+        'data' => 'date',
+    ];
 }
