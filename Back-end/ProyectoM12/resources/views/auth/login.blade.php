d34ec7ef9ff0bb4f1297279e6324db711e957e9c
Iván González Parra <ivan_gonzalezparra@iescarlesvallbona.cat>
2025-05-19 20:07:32 +0200

    Añadido la vista individual y mas detallada de los manuales

--- Back-end/ProyectoM12/resources/views/auth/login.blade.php
+++ Back-end/ProyectoM12/resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <div class="flex min-h-[80vh]">
         <!-- Lado del formulario -->
-        <div class="w-full max-w-xl flex flex-col justify-start items-center bg-white shadow-lg px-12 py-10 mt-12 rounded-lg">
+        <div class="w-full max-w-xl flex flex-col justify-center items-center bg-white shadow-lg px-12 py-10 my-auto rounded-lg">
             <!-- Logo -->
             <div class="mb-6">
                 <img src="{{ asset('img/logo.png') }}" alt="Logo" class="h-16 mx-auto" style="height:300px; width: 300px;">
@@ -15,14 +15,14 @@
 
                 <!-- Email Address -->
                 <div>
-                    <x-input-label for="email" :value="__('Email')" />
+                    <x-input-label for="email" :value="'Email'" />
                     <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
                     <x-input-error :messages="$errors->get('email')" class="mt-2" />
                 </div>
 
                 <!-- Password -->
                 <div class="mt-4">
-                    <x-input-label for="password" :value="__('Contraseña')" />
+                    <x-input-label for="password" :value="'Contraseña'" />
                     <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
                     <x-input-error :messages="$errors->get('password')" class="mt-2" />
                 </div>
@@ -31,26 +31,22 @@
                 <div class="block mt-4">
                     <label for="remember_me" class="inline-flex items-center">
                         <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                        <span class="ms-2 text-sm text-gray-600">{{ __('Recuerdame') }}</span>
+                        <span class="ms-2 text-sm text-gray-600">Recuerdame</span>
                     </label>
                 </div>
 
                 <div class="flex items-center justify-end mt-4">
                     @if (Route::has('password.request'))
                         <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                            {{ __('Has olvidado tu contraseña?') }}
+                            ¿Has olvidado tu contraseña?
                         </a>
                     @endif
 
                     <x-primary-button class="ms-3">
-                        {{ __('Iniciar sesión') }}
+                        Iniciar sesión
                     </x-primary-button>
                 </div>
             </form>
-        </div>
-        <!-- Lado de la imagen -->
-        <div class="hidden md:flex flex-1 items-center justify-center bg-gray-100">
-            <img src="{{ asset('img/warhammer_portada.png') }}" alt="Imagen lateral" class="object-cover h-full w-full">
         </div>
     </div>
 </x-guest-layout>
